0fd5b70d36dbcca5170f2edb0dca4ef2af5d7a66
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2019-11-21 10:46:53 -0500

    bug fixes

--- mycluster.py
+++ mycluster.py
@@ -57,11 +57,11 @@ class run_cluster(kernelcompare.KernelCompare):
         Ndiff_type_variations = ('modeldict:Ndiff_type', ['recursive', 'product'])
         max_bw_Ndiff_variations = ('modeldict:max_bw_Ndiff', [2])
         Ndiff_start_variations = ('modeldict:Ndiff_start', [1, 2])
-        product_kern_norm_variations = ('modeldict:product_kern_norm', ['self', 'own_n'])
+        #product_kern_norm_variations = ('modeldict:product_kern_norm', ['self', 'own_n'])
         #normalize_Ndiffwtsum_variations = ('modeldict:normalize_Ndiffwtsum', ['own_n', 'across'])
         # ykern_grid_variations=('ykern_grid',[31,46,61])
-        optdict_variation_list = [Ndiff_type_variations, max_bw_Ndiff_variations, Ndiff_start_variations,
-                                  product_kern_norm_variations]#, normalize_Ndiffwtsum_variations]
+        optdict_variation_list = [Ndiff_type_variations, max_bw_Ndiff_variations, Ndiff_start_variations]
+
         return optdict_variation_list
 
     def getdatagenvariations(self):
