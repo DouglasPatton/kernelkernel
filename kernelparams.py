6ecebe8cbb73583975ba3e25bc7c6da3c054af16
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2020-02-20 14:37:31 -0500

    updated params

--- kernelparams.py
+++ kernelparams.py
@@ -235,7 +235,7 @@ class KernelParams:
             'xatol':0.05,
             'fatol':.01,
             'adaptive':True,
-            'maxiter':50
+            'maxiter':10
             }
         optimizer_settings_dict1={
             'method':'Nelder-Mead',
