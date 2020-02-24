614a384dab8f6765ef9b1ea94d98cdd7b220c045
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2020-02-24 09:18:09 -0500

    runnode updated

--- runnode.py
+++ runnode.py
@@ -1,3 +1,12 @@
 if __name__=="__main__":
     import mycluster
-    mycluster.run_cluster(myname='node',local_test='no')
+    import psutil
+    cores=int(psutil.cpu_count(logical=False))
+    print(f'choose a node count between 1 and {cores-2}')
+    nodecount=int(input('node count:'))
+
+    mycluster.mypool(nodecount=nodecount,
+                source='pisces',
+                includemaster=0,
+                local_run=0
+               )
