950ca0d34a5916b5e2eca00040c59f1b519a75ff
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2020-02-27 16:56:51 -0500

    master fixes

--- mycluster.py
+++ mycluster.py
@@ -65,7 +65,7 @@ class run_cluster(kernelcompare.KernelCompare):
         kernelcompare.KernelCompare.__init__(self,directory=self.savedirectory,source=source,myname=myname)
         
         self.masterdirectory=self.setmasterdir(self.savedirectory)
-        self.oldnode_threshold=datetime.timedelta(minutes=10,seconds=1)
+        self.oldnode_threshold=datetime.timedelta(minutes=25,seconds=1)
         self.masterfilefilename=os.path.join(self.masterdirectory, 'masterfile')
         if myname is None:
             myname='node'
