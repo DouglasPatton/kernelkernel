1b5ee637e9b03534e8ef9f704643487d74d3c9ed
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2020-03-03 08:50:24 -0500

    bug fixes, logger added

--- xgboost_process.py
+++ xgboost_process.py
@@ -1,7 +1,21 @@
 from pisces_data_huc12 import PiscesDataTool
+import os
+import logging
 
 class XgboostProcessTool(PiscesDataTool):
     def __init__(self):
+        
+        self.cwd=os.getcwd()
+        self.logdir=os.path.join(self.cwd,'log')
+        if not os.path.exists(self.logdir):os.mkdir(self.logdir)
+        handlername=os.path.join(self.logdir,f'XgboostProcessTool.log')
+        logging.basicConfig(
+            handlers=[logging.handlers.RotatingFileHandler(handlername, maxBytes=10**7, backupCount=1)],
+            level=logging.DEBUG,
+            format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+            datefmt='%Y-%m-%dT%H:%M:%S')
+        self.logger = logging.getLogger(handlername)
+        
         PiscesDataTool.__init__(self)
         
         
