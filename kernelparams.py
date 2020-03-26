ab96d3dd2ac0446ee8198a919eb06351c2ed10ba
DouglasPatton <douglaspatton@gmail.com>
2020-03-26 16:02:39 -0400

    bug fix

--- kernelparams.py
+++ kernelparams.py
@@ -38,7 +38,7 @@ class KernelParams:
         
         Ndiff_outer_x_bw_startingvalue_variations=('hyper_param_dict:outer_x_bw',[np.array([i]) for i in np.linspace(.3,.7,4)])
         #Ndiff_outer_x_bw_startingvalue_variations=('hyper_param_dict:outer_x_bw',[np.array([.5])])
-        Ndiff_outer_y_bw_startingvalue_variations=('hyper_param_dict:outer_y_bw',[np.array([i]) for i in np.linspace(.3,.7,4])
+        Ndiff_outer_y_bw_startingvalue_variations=('hyper_param_dict:outer_y_bw',[np.array([i]) for i in np.linspace(.3,.7,4)])
         #Ndiff_outer_y_bw_startingvalue_variations=('hyper_param_dict:outer_y_bw',[np.array([.5])])
                                   
         NWnorm_variations=('modeldict:NWnorm',['none'])
