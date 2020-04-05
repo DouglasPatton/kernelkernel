00fbf45eb39051c814b19fb6e5a0c60d9bb6dd75
DP <douglaspatton@gmail.com>
2020-04-04 21:48:08 -0400

    batch_n an dbatch count bit change

--- kernelparams.py
+++ kernelparams.py
@@ -5,8 +5,8 @@ from pisces_data_huc12 import PiscesDataTool
 class KernelParams:
     
     def __init__(self,):
-        self.n=32 #used to generate variations datagen-batch_n and ykern_grid that are len n and n+1
-        self.batchcount_variation_list=[8]
+        self.n=8 #used to generate variations datagen-batch_n and ykern_grid that are len n and n+1
+        self.batchcount_variation_list=[128]
         self.do_minimize=0
         self.maxiter=3
         
