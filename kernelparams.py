a15992a6557c34aacce78f979ff36055cc235bd2
DP <douglaspatton@gmail.com>
2020-04-07 08:25:56 -0400

    params

--- kernelparams.py
+++ kernelparams.py
@@ -134,8 +134,8 @@ class KernelParams:
             
                 
                 
-            species_variations=('species',[self.specieslist[i] for i in range(0,2)])    
-            #species_variations=('species',[self.specieslist[i] for i in range(0,200)])
+            #species_variations=('species',[self.specieslist[i] for i in range(0,2)])    
+            species_variations=('species',[self.specieslist[i] for i in range(0,200)])
             #species_variations=('species',[self.specieslist[i] for i in range(20,100,2)])
             # print('species_variations',species_variations)
             #species_variations=('species',[self.specieslist[i] for i in range(0,len(self.specieslist)-11,11)])
