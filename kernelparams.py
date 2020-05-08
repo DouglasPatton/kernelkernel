54edada36c5854e86af22fe1ce4b897928d7e67b
Douglas Patton <douglaspatton@gmail.com>
2020-05-08 09:42:34 -0400

    ybandscale upto 5

--- kernelparams.py
+++ kernelparams.py
@@ -31,7 +31,7 @@ class KernelParams:
             startarray[i]=0.99
             arraylist.append(startarray)
         x_bandscale_startingvalue_variations=('hyper_param_dict:x_bandscale',arraylist)
-        y_bandscale_startingvalue_variations=('hyper_param_dict:y_bandscale',[np.array([i]) for i in [.5,1,2]])
+        y_bandscale_startingvalue_variations=('hyper_param_dict:y_bandscale',[np.array([i]) for i in [1,5]])
 
         #hyper_param_form_dict_variations=('modeldict:hyper_param_form_dict:x_bandscale',['fixed'])
         #Ndiff_exponentstartingvalue_variations=('hyper_param_dict:Ndiff_exponent',[factor1*np.array([1,1]) for factor1 in [.4,.5,.75,1]])
