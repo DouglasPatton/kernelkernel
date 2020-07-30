df37514849b52e802f80289842791e5885bf9141
DouglasPatton <douglaspatton@gmail.com>
2020-07-30 17:47:34 -0400

    add

---
+++ sk_tool.py
@@ -0,0 +1,24 @@
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import ElasticNetCV, LinearRegression
+from sklearn.svm import LinearSVR, SVR
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_validate, train_test_split, RepeatedKFold, GridSearchCV
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.ensemble import GradientBoostingRegressor
+
+class skTool()
+    def __init__(self,xtrain,xtest,ytrain,ytest):
+        linear_regression=make_pipeline(StandardScaler(),LinearRegression())
+        elastic_net = make_pipeline(StandardScaler(), ElasticNetCV())
+        #linear_svr = make_pipeline(StandardScaler(),GridSearchCV(LinearSVR(random_state=rs,tol=1e-3,max_iter=5000),n_jobs=1,param_grid={'C':np.linspace(-1,4,8)}))
+        linear_svr = make_pipeline(StandardScaler(),LinearSVR(random_state=rs,tol=1e-4,max_iter=5000,C=1))
+        rbf_svr=make_pipeline(StandardScaler(),SVR(kernel='rbf',tol=1e-4,max_iter=5000, C=1))
+        gradient_boosting_reg=make_pipeline(StandardScaler(),GradientBoostingRegressor())
+        model_dict={'linear-regression':linear_regression,
+            'elastic-net':elastic_net, 
+            'linear-svr':linear_svr, 
+            'rbf-svr':rbf_svr, 
+            'gradient-boosting-reg':gradient_boosting_reg}
+        for model in model_dict:
+            model.fit(xtrain,ytrain)
+            yhat=model.predict(xtest)
