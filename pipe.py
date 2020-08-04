cbb00f1d34482b76e743c735664f9939b82b0c09
Douglas Patton <douglaspatton@gmail.com>
2020-08-03 21:24:25 -0400

    f1_score in pipe

--- pipe.py
+++ pipe.py
@@ -21,7 +21,7 @@ class PipeLine(object):
             'overrides':[]
             }
         
-        self.sidestep_override_runlist=[[('modeldict:loss_function',lf)] for lf in ['mae','logloss','f1']]
+        self.sidestep_override_runlist=[[('modeldict:loss_function',lf)] for lf in ['mae','logloss','f1_score']]
         
     def buildsidestep_setupdictlist(self,sidestep_override_runlist,mainstep_setupdict,startstep=1):
         count=len(sidestep_override_runlist)
