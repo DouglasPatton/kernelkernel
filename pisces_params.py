3c4175f3b9a3bd3674ad56e4fc54307de21d3109
DP <douglaspatton@gmail.com>
2020-09-12 15:41:03 -0400

    pisces_params for model_setup and data_setup

---
+++ pisces_params.py
@@ -0,0 +1,22 @@
+
+class PiSetup:
+    def __init__(self,):
+    
+    
+    def model_setup(self,data_gen)
+    
+    def data_setup(self,)
+    
+    
+    
+    
+    
+    
+class MonteSetup:
+    def __init__(self,):
+    
+    
+    def model_setup(self,data_gen)
+    
+    def data_setup(self,)
+        
