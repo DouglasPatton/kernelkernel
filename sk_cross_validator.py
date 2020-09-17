0ba2b04c542c7ce5e19c2eacdc940defd0a65d47
DP <douglaspatton@gmail.com>
2020-09-17 18:16:01 -0400

    working through balanced samplingg

--- sk_cross_validator.py
+++ sk_cross_validator.py
@@ -1,11 +1,21 @@
 class CV_cat_reg_supreme
-    def (self,X_train,y_train,folds=10,reps=2,catreg='cat',strategy='balanced'):
+    def (self,X_train,y_train,cv_folds=10,cv_reps=2,cat_reg='cat',strategy='balanced'):
         #needs updating!
         
         # test data already removed
-        batchbatchcount=self.batchbatchcount
-        n=ydataarray.shape[0]; p=xdataarray.shape[1]
+        n,k=X_train.shape
         onecount=int(ydataarray.sum())
+        sum_count_arr=np.ones([n,])
+        if cat_reg=='cat':
+            cats=np.unique(y_train)
+            cat_count_dict={}
+            for cat in cats:
+                cat_count_dict[cat]=np.sum(sum_count_arr[y_train==cat])
+        elif cat_reg=='reg':
+            
+            cat_edges=np.quantile(y_train,np.linspace(0,1,6))
+            q=np.quantile(y_train)    
+            bins,_=np.histogram(y_train,bins=q)
         zerocount=n-onecount
         countlist=[zerocount,onecount]
         if onecount<zerocount:
@@ -27,7 +37,7 @@ class CV_cat_reg_supreme
             oneidx=np.arange(n)[ydataarray==1]
             zeroidx=np.arange(n)[ydataarray==0]
             bb_select_list=[]
-            for bb_idx in range(batchbatchcount):
+            for bb_idx in range(cv_reps):
                 ones=np.random.choice(oneidx,size=batch01_n[1]*batchcount,replace=False)
                 zeros=np.random.choice(zeroidx,size=batch01_n[0]*batchcount,replace=False)
                 bb_select_list.append(np.concatenate([ones,zeros],axis=0))
@@ -37,11 +47,11 @@ class CV_cat_reg_supreme
                 #self.logger.info(f'for {self.species} batchcount changed from {batchcount} to {max_batchcount}')
                 batchcount=max_batchcount
             subsample_n=batchcount*batch_n
-            for bb_idx in range(batchbatchcount):
+            for bb_idx in range(cv_reps):
                 bb_select_list.append(np.random.choice(np.arange(n),size=subsample_n),replace=False)
-        batchbatchlist=[[None for __ in range(batchcount)] for _ in range(batchbatchcount)]
+        batchbatchlist=[[None for __ in range(batchcount)] for _ in range(cv_reps)]
         SKF=StratifiedKFold(n_splits=batchcount, shuffle=False)
-        for bb_idx in range(batchbatchcount):
+        for bb_idx in range(cv_reps):
             bb_x_subsample=xdataarray[bb_select_list[bb_idx],:]
             bb_y_subsample=ydataarray[bb_select_list[bb_idx]]
             for j,(train_index,test_index) in enumerate(SKF.split(bb_x_subsample,bb_y_subsample)):
@@ -51,7 +61,7 @@ class CV_cat_reg_supreme
         
         self.batchcount=batchcount
         self.expand_datagen_dict('batchcount',self.batchcount)
-        fullbatchbatch_n=batchbatchcount*batchsize
+        fullbatchbatch_n=cv_reps*batchsize
         self.fullbatchbatch_n=fullbatchbatch_n
         self.expand_datagen_dict('fullbatchbatch_n',self.fullbatchbatch_n)
         self.logger.info(f'yxtup shapes:{[(yxtup[0].shape,yxtup[1].shape) for yxtuplist in batchbatchlist for yxtup in yxtuplist]}')
