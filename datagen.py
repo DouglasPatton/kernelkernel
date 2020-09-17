9bb65aa3b762e8c0e9a05eb25f09be94b4e5f2b8
DP <douglaspatton@gmail.com>
2020-09-17 11:43:08 -0400

    laptop

--- datagen.py
+++ datagen.py
@@ -4,9 +4,10 @@ import numpy as np
 from pisces_data_huc12 import PiscesDataTool
 import os
 import sklearn
-from sklearn.model_selection import StratifiedKFold,train_test_split
+from sklearn.model_selection import StratifiedKFold,RepeatedKFold,train_test_split
 from sklearn.preprocessing import StandardScaler
 from mylogger import myLogger
+
 class datagen(PiscesDataTool,myLogger):
     '''
     
@@ -16,18 +17,29 @@ class datagen(PiscesDataTool,myLogger):
         myLogger.__init__(self,name='datagen.log')
         self.logger.info('starting new datagen log')
         source=datagen_dict['source']
-        self.datagen_dict=datagen_dict
+        datagen_dict=self.datagen_dict
+        PiscesDataTool.__init__(self,)
+        species=datagen_dict['species']
+        if type(species) is str:
+            self.fulldata=retrievespeciesdata(species_name=species)
+        elif type(species) is int:
+            self.fulldata=retrievespeciesdata(species_idx=species)
         
-        
-        if source=='pisces':
-            PiscesDataTool.__init__(self,)
-            species=datagen_dict['species']
-            test_share=0.1
-            cv_folds=5
-            cv_reps=2
-            stratify=None#'balanced-HUC8'
-            inner_cv_folds=5
-            inner_cv_reps=5
-            cv_count=cv_folds*cv_reps
-            
+        self.cv=RepeatedKFold(n_splits=cv_folds, n_repeats=cv_reps, random_state=rs)    
+        y_name='presence'
+        self.loc_vars=['HUC12']
+        drop_vars=datagen_dict['drop_vars']
+        drop_vars.extend(self.loc_vars)
+        drop_vars.append(y_name)
+        self.x_vars=[var for var in all_vars if var not in drop_vars]
+        X_df=df.loc[:,self.x_vars]
+        y_df=df.loc[:,y_name]
+        if test_share:
+            X_train, X_test, y_train, y_test = train_test_split(X_df, y_df, test_size=test_share, random_state=rs)
+        else:
+            X_train, X_test, y_train, y_test = (X_df, None, y_df, None)
+        self.X_train=X_train
+        self.X_test=X_test
+        self.y_train=y_train
+        self.y_test=y_test
         
--- pisces_params.py
+++ pisces_params.py
@@ -4,19 +4,32 @@ from mylogger import myLogger
 import logging
 
 class PiSetup:
-    def __init__(self,f
-        floatselecttup=(3,5,6)
-        spatialselecttup=(8,)
+    def __init__(self,)
         pdt=PiscesDataTool()
-        
-    
+        self.modelsetupdict=dict(
+            gridpoints=5,
+            inner_cv_folds=5,
+            inner_cv_reps=5,
+            )
+        self.initial_datagen_dict=dict(
+            species=None, # set in data_setup
+            data_split=dict(
+                test_share=0.1,
+                cv=dict(
+                    cv_folds=5,
+                    cv_reps=2,
+                    ),
+                drop_vars=[],
+                stratify=None,#'balanced-HUC8'
+                )
+            )
     
     def model_setup(self,):
         #sk_tool uses model_gen to create the estimator
         est_dict=SkTool().get_est_dict()
         model_gen_list=[]
         for est_name in est_dict.keys():
-            kwargs={'gridpoints':5}
+            kwargs=self.model_setup_dict
             model_gen={'kwargs':kwargs,'name':est_name}
             model_gen_list.append(model_gen)
         return model_gen_list
@@ -24,17 +37,8 @@ class PiSetup:
     
     def data_setup(self,):
         species_list=pdt.returnspecieslist()
-        data_params={
-            'source':'pisces'
-            'species':None
-            'test_share':0.1,
-            'cross_validate':False,
-            'missing':'drop_row', #drop the row(observation) if any data is missing
-            'floatselecttup':floatselecttup,
-            'spatialselecttup':spatialselecttup,
-            'param_count':param_count,
-            'sample':'y-balanced'
-        }
+        cvdict=self.cvdict
+        data_params=self.datagen_dict
         
         data_gen_list=[]
         for species in species_list:
