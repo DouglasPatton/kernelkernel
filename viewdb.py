f1a8c0584c972f6182061da5a95bf0bf02da9173
Douglas Patton <douglaspatton@gmail.com>
2020-09-25 21:18:58 -0400

    view details

--- viewdb.py
+++ viewdb.py
@@ -5,3 +5,25 @@ if __name__ == "__main__":
     gen_dict=dbt.genDBdict()
     print('len(results_dict): ',len(results_dict))
     print('len(gen_dict) ',len(gen_dict))
+    success=0
+    fail=0
+    for key in results_dict.keys():
+        try:
+            r=results_dict[key]
+            success+=1
+        except:
+            fail+=1
+    print('resultsdict:')
+    print('successes: ',success)
+    print('failures: ',fail)
+    success=0
+    fail=0
+    for key in gen_dict.keys():
+        try:
+            r=gen_dict[key]
+            success+=1
+        except:
+            fail+=1
+    print('gendict:')
+    print('successes: ',success)
+    print('failures: ',fail)
