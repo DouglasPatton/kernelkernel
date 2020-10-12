bdb8b08fde9dc5a85e9b25dd9fd57ff4e1925950
Douglas Patton <douglaspatton@gmail.com>
2020-10-12 12:36:02 -0400

    add

---
+++ pi_map.py
@@ -0,0 +1,21 @@
+import os
+
+class Mapper:
+    def__init__(self):
+        cwd=os.getcwd()
+        self.geo_data_dir=os.path.join(cwd,'geo_data')
+        self.print_dir=os.path.join(cwd,'print')
+        self.boundary_data_check()
+        
+        
+    def boundary_check(self):
+        datalink="https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/WBD/National/GDB/WBD_National_GDB.zip"
+        boundary_data_path=os.path.join(self.geo_data_dir,'WBD_National_GDB.gdb')
+        if not os.path.exists(boundary_data_path):
+            assert False, f"cannot locate boundary data. download from {datalink}"
+        
+            
+            
+    
+
+
