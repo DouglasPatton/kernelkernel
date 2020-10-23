a9d475255dd42550da5c995c30ec0508f15f03a7
Douglas Patton <douglaspatton@gmail.com>
2020-10-23 12:53:33 -0400

    wts finished for now, working on plot top features

--- pi_map.py
+++ pi_map.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process,Queue
 from time import time,sleep
+import re
 import os
 import geopandas as gpd
 import pandas as pd
@@ -13,53 +14,52 @@ from mylogger import myLogger
 
 
 class MatchCollapseHuc12(Process,myLogger):
-    def __init__(self,q,huc12list,coef_a,fitscor,adiff_scor_chunk):
-        myLogger.__init__(self,name='MatchCollapseHuc12.log')
-        self.logger.info(f'starting up a MatchCollapseHuc12 proc')
+    def __init__(self,q,huc12list,coef_df,fitscor,adiff_scor_chunk):
+        #myLogger.__init__(self,name='MatchCollapseHuc12.log')
+        #self.logger.info(f'starting up a MatchCollapseHuc12 proc')
         super().__init__()
         self.q=q
         self.huc12list=huc12list
-        self.coef_a=coef_a
+        self.coef_df=coef_df
         self.fitscor=fitscor
         self.adiff_scor_chunk=adiff_scor_chunk
     
     def run(self,):
+        myLogger.__init__(self,name='MatchCollapseHuc12.log')
+        self.logger.info(f'running a MatchCollapseHuc12 proc')
+        pid=os.getpid()
+        self.logger.info(f'mch is running at pid:{pid}')
         try:
             dflist=[]
-            for huc12 in self.huc12list[:1]:
-                huc12_adiff_scor=self.adiff_scor_chunk.loc[(slice(None),huc12,slice(None),slice(None)),:]
+            hcount=len(self.huc12list)
+            blocksize=100
+            blockcount=-(-hcount//blocksize) # ceiling divide
+            block_selector=[(blocksize*i,blocksize*(i+1))for i in range(blockcount)]
+            for block_idx in block_selector:
+                self.logger.info(f'pid:{pid} on block_idx:{block_idx}')
+                huc12block=self.huc12list[block_idx[0]:block_idx[1]]
+            #######
+                huc12_adiff_scor=self.adiff_scor_chunk.loc[(slice(None),huc12block,slice(None),slice(None)),:]
+                huc12_adiff_scor.index=huc12_adiff_scor.index.remove_unused_levels()
                 self.huc12_adiff_scor=huc12_adiff_scor
                 #this should add comids and huc12s to fitscor
                 _,fitscor=huc12_adiff_scor.align(self.fitscor,axis=0,join='left')
-                #huc12_adiff_scor,fitscor=huc12_adiff_scor.align(fitscor,axis=1)
-                #fitscor.columns=fitscor.columns.droplevel('var')#already done
                 wt=huc12_adiff_scor.multiply(fitscor,axis=0)
                 self.wt=wt
                 denom=wt.sum(axis=1,level='var')#.sum(axis=0,level='HUC12')
                 denom_a,_=denom.align(wt,axis=1,level='var')
-                #denom__a,_=denom_a.align(wt,axis=0,level='HUC12')
-                #denom_a.columns=denom_a.columns.droplevel('var')
                 normwt_var=wt.divide(denom_a.values)
                 self.denom=denom;self.denom_a=denom_a;#self.denom_aa=denom_aa
                 self.normwt_var=normwt_var
                 self.huc12_adiff_scor=huc12_adiff_scor
-                
-                '''denom=huc12_adiff_scor.sum(axis=1,level='var').sum(axis=0,level='HUC12')
-                #self.logger.info(f'denom1:{denom}')
-                huc12_adiff_scor,denom=huc12_adiff_scor.align(denom,axis=0)
-                #self.logger.info(f'denom2:{denom}')
-                huc12_adiff_scor,denom=huc12_adiff_scor.align(denom,axis=1)
-                #self.logger.info(f'denom3:{denom}')
-                normed_huc12_adiff_scor=huc12_adiff_scor.divide(denom.values,axis=0)
-                self.logger.info(f'normed_huc12_adiff_scor1:{normed_huc12_adiff_scor}')'''
-                coef_a=self.coef_a
+                coef_df=self.coef_df
                 normwt_var.columns=normwt_var.columns.droplevel('var')
-                _,normwt_var=coef_a.align(normwt_var,axis=0)
-                _,normwt_var=coef_a.align(normwt_var,axis=1)
-                wt_coef=coef_a.mul(normwt_var,axis=0)
+                _,normwt_var=coef_df.align(normwt_var,axis=0)
+                _,normwt_var=coef_df.align(normwt_var,axis=1)
+                wt_coef=coef_df.mul(normwt_var,axis=0)
                 varwt_coef=wt_coef.sum(axis=1,level='var')#.sum(axis=0,level='HUC12')
                 self.varwt_coef=varwt_coef
-                
+
                 wtmean=wt.mean(axis=1,level='var') #for combining across ests/specs, 
                 self.wtmean=wtmean
                 ##get average weight for each coef
@@ -68,26 +68,27 @@ class MatchCollapseHuc12(Process,myLogger):
                 #denom2_a.columns=denom2_a.columns.droplevel('var')
                 self.denom2=denom2;self.denom2_a=denom2_a
                 normwt_huc=wtmean.divide(denom2_a,axis=0)
-                
+
                 self.normwt_huc=normwt_huc
                 _,normwt_huc=varwt_coef.align(normwt_huc,axis=0)
                 #normwt_huc.columns=normwt_huc.columns.droplevel('var')
                 wt2_coef=varwt_coef.mul(normwt_huc,axis=0)
                 hucwt_varwt_coef=varwt_coef.sum(axis=0,level='HUC12')
-                """
-                huc12_coefs,normed_huc12_adiff_scor=self.scorwtd_coef_df.align(
-                    normed_huc12_adiff_scor,join='right',axis=0)
-                self.logger.info(f'normed_huc12_adiff_scor2:{normed_huc12_adiff_scor}')
-                huc12_coefs,normed_huc12_adiff_scor=huc12_coefs.align(
-                    normed_huc12_adiff_scor.loc[:,('y',slice(None),slice(None))]
-                    ,join='left',axis=1)
-                self.logger.info(f'normed_huc12_adiff_scor3:{normed_huc12_adiff_scor}')"""
+                dflist.append(hucwt_varwt_coef)
+            ######## end former for loop
+            self.q.put(dflist)
+            """t=0
+            while t<5
+                try:
+                    self.q.put(hucwt_varwt_coef,10) #since no dflist w/o loop
+                    return
+                except:
+                    self.logger.exception(f'error adding to q')
+                    t+=1
+            self.logger.error(f'failed to add job to q')    
+            self.logger.error(f'hucwt_varwt_coef:{hucwt_varwt_coef}')
                 
-                dflist.append(
-                    hucwt_varwt_coef)
-            chunk_df=pd.concat(dflist,axis=0)
-            self.q.put(chunk_df)
-            return chunk_df
+            #return chunk_df"""
         except:
             self.logger.exception('')
         
@@ -125,7 +126,7 @@ class Mapper(myLogger):
                 countdown-=1
                 self.logger.info(f'proc completed. countdown:{countdown}')
             except:
-                self.logger.exception('error')
+                #self.logger.exception('error')
                 if not q.empty(): self.logger.exception(f'error while checking q, but not empty')
                 else: sleep(5)
         [proc.join() for proc in procs]
@@ -135,17 +136,20 @@ class Mapper(myLogger):
         self.logger.info(f'pool complete at {endtime}, time elapsed: {(endtime-starttime)/60} minutes')
         return outlist
     
-    def build_wt_comid_feature_importance(self,rebuild=0,fit_scorer='f1_micro'):#,wt_kwargs={'norm_index':'COMID'}):
+    def build_wt_comid_feature_importance(self,rebuild=0,fit_scorer='f1_micro',zzzno_fish=False):#,wt_kwargs={'norm_index':'COMID'}):
         #get data
-        
         name='wt_comid_feature_importance'
-        if False:#not rebuild: #just turning off rebuild here
+        if zzzno_fish:
+            name+='zzzno fish'
+            
+        if not rebuild: #just turning off rebuild here
             try:
                 saved_data=self.pr.getsave_postfit_db_dict(name)
                 return saved_data['data']#sqlitedict needs a key to pickle and save an object in sqlite
             except:
                 self.logger.info(f'rebuilding {name} but rebuild:{rebuild}')
         datadict=self.pr.stack_predictions(rebuild=rebuild)
+        
         y=datadict['y']#.astype('Int8')
         yhat=datadict['yhat']#.astype('Int8')
         coef_scor_df=datadict['coef_scor_df']#.astype('float32')
@@ -184,35 +188,25 @@ class Mapper(myLogger):
         #wt ceofs w/ score
         self.scor_select=scor_select
         self.coef_df=coef_df
-        #assert False,'pause'
-        ####
-        ####
-        ####
+
         scor_select.columns=scor_select.columns.droplevel('var')
-        coef_a,scor_a=coef_df.align(scor_select,axis=1)
-        #normed_scor=scor_a.divide(scor_a.sum(axis=1,level='var'),axis=0)
-        #scorwtd_coef_df=coef_a.multiply(normed_scor,axis=0)        #
-        
-        
-        
-        
+        #coef_a,scor_a=coef_df.align(scor_select,axis=1)
         """need to drop zzzno_fish before summing for weight normalization"""
-        #broadcast coefs to huc12/comid levels
-        #self.scorwtd_coef_df=scorwtd_coef_df
-        #self.adiff_scor=adiff_scor
-        #assert False,'pausecheck'
-        #scorwtd_coef_df_a,adiff_a=scorwtd_coef_df.align(adiff,axis=0,join='right',level=['species','estimator'])
-        #wtd_coef_df=scorwtd_coef_df_a.multiply(adiff_a,axis=0)
-        #wtd_coef_df=pd.DataFrame()
-        proc_count=15
+        if zzzno_fish:
+            assert False,'not developed'
+        else:
+            scor_select.drop('zzzno fish',level='species',inplace=True)
+            coef_df.drop('zzzno fish',level='species',inplace=True)
+            adiff_scor.drop('zzzno fish',level='species',inplace=True)
+        #break into chunks and line up coefs w/ huc12's in groups b/c 
+        proc_count=10
         huc12_list=y.index.levels[1].to_list()
         chunk_size=-(-len(huc12_list)//proc_count) # ceiling divide
         huc12chunk=[huc12_list[chunk_size*i:chunk_size*(i+1)] for i in range(proc_count)]
         adiff_scor_chunk=[adiff_scor.loc[(slice(None),huc12chunk[i],slice(None),slice(None)),:] for i in range(proc_count)]
-        #scor_coef_chunk=[scorwtd_coef_df.loc[(slice(None),huc12chunk[i],slice(None),slice(None)),:] for i in range(proc_count)]
-        args_list=[[huc12chunk[i],coef_a,scor_select,adiff_scor_chunk[i]] for i in range(proc_count)]
+        args_list=[[huc12chunk[i],coef_df,scor_select,adiff_scor_chunk[i]] for i in range(proc_count)]
         q=Queue()
-        self.mch_list=[]
+        """self.mch_list=[]
         self.results=[]
         for args in args_list[:1]:
             args=[q,*args]
@@ -220,16 +214,16 @@ class Mapper(myLogger):
             self.mch_list.append(mch)
             self.results.append(self.mch_list[-1].run())
         
-        wtd_coef_df=pd.concat(self.results,axis=0)
-        #dflist=self.runAsMultiProc(MatchCollapseHuc12,args_list)
-        '''for huc12 in huc12_list:
-            huc12_adiff_scor=adiff_scor.loc[(slice(None),huc12,slice(None),slice(None)),:]
-            huc12_coefs,huc12_adiff_scor=scorwtd_coef_df.align(huc12_adiff_scor,join='right',axis=0)
-            dflist.append(
-                huc12_coefs.mul(huc12_adiff_scor).sum(axis=1,level='var').sum(
-                    axis=0,level='HUC12'))
+        wtd_coef_df=pd.concat(self.results,axis=0)"""
+        print(f'starting {proc_count} procs')
+        dflistlist=self.runAsMultiProc(MatchCollapseHuc12,args_list)
+        print('multiprocessing complete')
+        dflist=[]
+        for dfl in dflistlist:
+            dflist.extend(dfl)
         self.dflist=dflist
-        wtd_coef_df=pd.concat(dflist,axis=0)  '''
+        print('concatenating dflist')
+        wtd_coef_df=pd.concat(dflist,axis=0)  
         
             
         #wtd_coef_df=scorwtd_coef_df.multiply(adiff,axis=0)
@@ -270,9 +264,13 @@ class Mapper(myLogger):
         wtd_coef_df=self.build_wt_comid_feature_importance(rebuild=rebuild)
         if split is None:
             cols=wtd_coef_df.columns
+            drop_vars=['tmean','tmax','msst','mwst','precip','slope','wa','elev','mast','tmin']
             for col in cols:
-                if col[:2]=='Tm':
-                    wtd_coef_df.drop(col,axis=1,inplace=True)
+                for varstr in drop_vars:
+                    if re.search(varstr,col.lower()):
+                        wtd_coef_df.drop(col,axis=1,inplace=True)
+                        self.logger.info(f'dropped col: {col}')
+                        break #stop searching
             big_mean=wtd_coef_df.mean(axis=0)
             big_cols_sorted=list(big_mean.sort_values().index)[::-1]#descending
             big_top_2n=(big_cols_sorted[:top_n],
@@ -281,7 +279,7 @@ class Mapper(myLogger):
             top_neg_coef_df=wtd_coef_df.loc[:,big_top_2n[1]]
             #self.big_top_wtd_coef_df=big_top_wtd_coef_df
             pos_sort_idx=np.argsort(top_pos_coef_df,axis=1).iloc[:,::-1]
-            neg_sort_idx=np.argsort(top_pos_coef_df,axis=1).iloc[:,::-1]
+            neg_sort_idx=np.argsort(top_neg_coef_df,axis=1)#.iloc[:,::-1]
             #select the best and worst columns
             big_top_cols_pos=pos_sort_idx.apply(
                 lambda x: big_top_2n[0][x[0]],axis=1)
@@ -298,15 +296,15 @@ class Mapper(myLogger):
                
             fig=plt.figure(dpi=300,figsize=[10,14])
             ax=fig.add_subplot(2,1,1)
-            divider = make_axes_locatable(ax)
-            cax = divider.append_axes("right", size="5%", pad=0.1)
-            geo_pos_cols.plot(column=colname,ax=ax,cax=cax,legend=True)#,legend_kwds={'orientation':'vertical'})
+            #divider = make_axes_locatable(ax)
+            #cax = divider.append_axes("right", size="5%", pad=0.1)
+            geo_pos_cols.plot(column=colname,ax=ax,cmap='tab20c',legend=True)#,legend_kwds={'orientation':'vertical'})
             self.add_huc2_conus(ax)
             
             ax=fig.add_subplot(2,1,2)
-            divider = make_axes_locatable(ax)
-            cax = divider.append_axes("right", size="5%", pad=0.1)
-            geo_neg_cols.plot(column=colname,ax=ax,cax=cax,legend=True)#,legend_kwds={'orientation':'vertical'})
+            #divider = make_axes_locatable(ax)
+            #cax = divider.append_axes("right", size="5%", pad=0.1)
+            geo_neg_cols.plot(column=colname,ax=ax,cmap='tab20c',legend=True)#,legend_kwds={'orientation':'vertical'})
             self.add_huc2_conus(ax)
     
             
@@ -362,7 +360,7 @@ class Mapper(myLogger):
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.1)
        
-        huc12_coef_gdf.plot(column=columns[colsort[0]],ax=ax,cax=cax,legend=True)#,legend_kwds={'orientation':'vertical'})
+        huc12_coef_gdf.plot(column=columns[colsort[0]],ax=ax,cax=cax ,legend=True)#,legend_kwds={'orientation':'vertical'})
         self.add_huc2_conus(ax)
         fig.savefig(Helper().getname(os.path.join(self.print_dir,'huc12_features.png')))
 
