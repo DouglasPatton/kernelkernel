91ad034932ea1dbcd828104feb35e3573a4ac829
douglaspatton@gmail.com <douglaspatton@gmail.com>
2020-11-25 12:32:55 -0500

    ready to test xpredict

--- datagen.py
+++ datagen.py
@@ -32,7 +32,7 @@ class dataGenerator(PiscesDataTool,myLogger):
         self.datagen_dict=datagen_dict
         try:
             try:
-                random_state=self.datagen_dict['random_state']
+                random_stafte=self.datagen_dict['random_state']
                 np.random.seed(random_state)
             except KeyError:
                 np.random.seed(0)
