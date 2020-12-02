c8d9d5156f03c5342c557d7f783f3710467d08b9
douglaspatton@gmail.com <douglaspatton@gmail.com>
2020-12-02 12:59:08 -0500

    fitfailDB len included in viewdb

--- viewdb.py
+++ viewdb.py
@@ -4,8 +4,10 @@ if __name__ == "__main__":
     dbt=DBTool()
     results_dict=dbt.resultsDBdict()
     gen_dict=dbt.genDBdict()
+    fitfail_dict=dbt.fitfailDBdict()
     print('len(resultsDBdict): ',len(results_dict))
-    print('len(genDBdict) ',len(gen_dict))
+    print('len(genDBdict): ',len(gen_dict))
+    print('len(fitfailDBdict): ',len(fitfail_dict))
     if os.path.exists(dbt.predictDBdictpath):
         print('len(predictDBdict)',len(dbt.predictDBdict()))
     success=0
